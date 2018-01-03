4e969a1d3e2d2adb1644eb4540fde5328d341621
Rufus Ben Abu <exile744@live.com>
2018-01-03 18:44:50 +0530

    Added Button Functionalities
    Added functionalities for the Submit, Clear and Close buttons

--- GUI.py
+++ GUI.py
@@ -12,29 +12,51 @@ from kivy.uix.image import AsyncImage
 import scraper
 
 class HelloApp(App): # build returns window content, here Label
+	def clearSearch(self, argx):
+		self.searchField.text = ""
+
+	def submit(self, argx):
+		print(self.searchField.text)
+		try:
+			try:
+				self.layout.remove_widget(self.img)
+			except Exception as e:
+				print(e)
+			self.img = AsyncImage(pos_hint = {'x': .35, 'center_y': 0.5}, source = scraper.getImg(self.searchField.text))
+			self.layout.add_widget(self.img)
+		except Exception as e:
+			print(e)
+
+	def close(self, argx):
+		App.get_running_app().stop()
+
 	def build(self): # Overriding the built in build method in App
 		self.layout = FloatLayout()
 
 		self.searchField = TextInput(pos_hint={'x': 0, 'center_y': 0.08}, size_hint = (.7, .05), multiline = False)
-		bLabels = ['Submit', 'Clear', 'Close']
+		bProperties = {'Labels' : ['Submit', 'Clear', 'Close'],
+					   'Functions' : [self.submit, self.clearSearch, self.close]}
+
 		self.mainButtons = []
 		self.mainContent = TextInput(pos_hint={'x': 0, 'center_y': 0.5 }, size_hint = (.7, .5), multiline = True, text = scraper.getWiki(), readonly = True, auto_indent = True, cursor_blink = False)
-		self.img = AsyncImage(pos_hint = {'x': .35, 'center_y': 0.5}, source = scraper.getImg())
 
 		for i in range(3):
-			self.mainButtons.append(Button(pos_hint = {'x': .70 + (i * 0.1), 'center_y': 0.08}, size_hint=(.1, .05),text= bLabels[i]))
+			self.mainButtons.append(Button(pos_hint = {'x': .70 + (i * 0.1), 'center_y': 0.08}, size_hint=(.1, .05),text= bProperties['Labels'][i]))
+			self.mainButtons[i].bind(on_press = bProperties['Functions'][i])
 			self.layout.add_widget(self.mainButtons[i])
 
 		self.layout.add_widget(self.searchField)
 		self.layout.add_widget(self.mainContent)
-		self.layout.add_widget(self.img)
+
 		return self.layout
 
-	def getText(self):
-		# TODO: Use this function to get the text from the text entry field. Pass it to get image function in scrapper
-		# then return the url and pass it to image widget, redraw image widget each time button is clicked with new
-		# image. If empty, don't draw image widget (same applies for big entry).
 
+
+	# def getText(self):
+	# 	# TODO: Use this function to get the text from the text entry field. Pass it to get image function in scrapper
+	# 	# then return the url and pass it to image widget, redraw image widget each time button is clicked with new
+	# 	# image. If empty, don't draw image widget (same applies for big entry).
+	# 	pass
 
 if __name__ == "__main__":
 	HelloApp().run() # Running an instance of HelloApp
--- scraper.py
+++ scraper.py
@@ -2,29 +2,31 @@ from bs4 import BeautifulSoup as soup
 from urllib import request
 import urllib
 
-# card_name = input("Enter the name of the card: ")
+def getPage(card_name): 
+	card_name = card_name.title()
+	print(card_name)
+	wikiUrl = 'https://hearthstone.gamepedia.com/{}'.format(card_name.strip())
+	wikiUrl = urllib.parse.quote(wikiUrl, safe=":/")
 
-card_name = 'Kingsbane'
-wikiUrl = 'https://hearthstone.gamepedia.com/{}'.format(card_name)
-wikiUrl = urllib.parse.quote(wikiUrl, safe=":/")
+	print(wikiUrl)
 
-pwnSearchUrl = "http://www.hearthpwn.com/search?search={}#t1:cards".format(card_name)
-pwnSearchResult = request.urlopen(pwnSearchUrl)
-
-try:
+	pwnSearchUrl = "http://www.hearthpwn.com/search?search={}#t1:cards".format(card_name)
+	pwnSearchResult = request.urlopen(pwnSearchUrl)
 
 
-wiki_web_page = request.urlopen(wikiUrl)
-wiki_content = wiki_web_page.read()
-wiki_web_page.close()
-wiki_page_soup = soup(wiki_content, "html.parser")
+	wiki_web_page = request.urlopen(wikiUrl)
+	wiki_content = wiki_web_page.read()
+	wiki_web_page.close()
+	wiki_page_soup = soup(wiki_content, "html.parser")
+	return wiki_page_soup
 
 def getWiki():
 	# mainContent = page_soup.find('div',{'id':'mw-content-text', 'class': 'mw-content-ltr'})
 	# container = mainContent.find('span', {'class': 'mw-headline', 'id': 'Strategy'})
-
 	return "Some random crap. Blah!"
 
-def getImg():
+def getImg(card_name):
+	wiki_page_soup = getPage(card_name)
 	imgContent = wiki_page_soup.find('div', {'class': 'image'})
 	return imgContent.a.img['src']
+
